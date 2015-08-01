8d1626119ccd00a565dad47f3602455420fda1b4
liqing <cendy172@gmail.com>
2015-08-01 15:18:55 +0800

    annotation based configuration.

--- src/main/java/com/thoughtworks/bean/Config.java
+++ src/main/java/com/thoughtworks/bean/Config.java
@@ -1,5 +1,10 @@
 package com.thoughtworks.bean;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value = "com.thoughtworks.bean")
 public class Config {
 
 }
--- src/main/java/com/thoughtworks/bean/Main.java
+++ src/main/java/com/thoughtworks/bean/Main.java
@@ -1,18 +1,18 @@
 package com.thoughtworks.bean;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("------------ applicationContext loading...");
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
         System.out.println("------------ applicationContext loading finish");
 
         System.out.println("------------ get bean calculatorClient");
-        CalculatorClient calculatorClient = (CalculatorClient) applicationContext.getBean("calculatorClient");
+        CalculatorClient calculatorClient = (CalculatorClient) applicationContext.getBean(CalculatorClient.class);
 
         System.out.println(calculatorClient.add("12", "35"));
         System.out.println(calculatorClient.add("112", "35"));
